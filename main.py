361b7be24fc61562b538c3ab6620dfbdab17b8cd
ashes999 <ashes999@yahoo.com>
2017-08-04 16:52:39 -0400

    Primitive card aggregation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import random
 
 from prototype.entities.champion import Champion
+from prototype.entities.cards.card import Card
 
 class Main:
     # Main entry point!
@@ -13,20 +14,38 @@ class Main:
     def load_json_data(self):
         # Load JSON data from files
 
-        with open('data/weapons.json') as data:
-            weapons = json.load(data)
-        
+       
+        with open('data/actions.json') as data:
+            actions = json.load(data)
+
         with open('data/armour.json') as data:
             armour = json.load(data)
 
         with open('data/champions.json') as data:
             champions = json.load(data)
+        
+        with open('data/consumables.json') as data:
+            consumables = json.load(data)
 
+        with open('data/skills.json') as data:
+            skills = json.load(data)
+        
+        with open('data/weapons.json') as data:
+            weapons = json.load(data)
+        
         # Populate class instances from said data
         self.champions = []
         for champ_data in champions:
             c = Champion(champ_data["name"], champ_data["health"], champ_data["weapon"], champ_data["armour"])
             self.champions.append(c)
+
+        self.cards = []
+        # Consumables show up several times because they're useful.
+        for item in actions + armour + consumables + consumables + consumables + skills + weapons:
+            c = Card(item["name"])
+            self.cards.append(c)
+
+        print(self.cards)
 
     def pick_champions(self):
         self.player = self.champions[0]
