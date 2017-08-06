b24a2f5b6021476d058779b6adb09324499e8a32
ashes999 <ashes999@yahoo.com>
2017-08-05 20:35:29 -0400

    Implement consumables

--- prototype/entities/cards/consumable.py
+++ prototype/entities/cards/consumable.py
@@ -4,3 +4,17 @@ class Consumable(Card):
     def __init__(self, json):
         super().__init__(json["name"])
         self.effect = json["effect"]
+
+    def apply(self, consumer, opponent):
+        index = self.effect.index(" ")
+        apply = self.effect[0:index].strip().lower()
+        amount = int(self.effect[index:].strip().replace('+', ''))
+
+        if apply == "health":
+            consumer.current_health += amount
+            print("{0} heals {1} health!".format(consumer.name, amount))
+        elif apply == "skillpoints":
+            consumer.skill_points += amount
+            print("{0} heals {1} skill points!".format(consumer.name, amount))
+        else:
+            raise NotImplementedError("Not sure how to consume a '{0}' consumable".format(apply))
